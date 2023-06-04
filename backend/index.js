53d7b0554924f0d50be91bd5199b22187b157a9f
Sam Verdejo <sjverdejo@gmail.com>
2023-06-04 13:45:58 -0400

    created a simple web server

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
+
