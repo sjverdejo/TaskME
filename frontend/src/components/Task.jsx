39831923aa3518c0b331c82135cdd4414658bdb7
Sam Verdejo <sjverdejo@gmail.com>
2023-06-05 23:36:53 -0400

    Added styling for task box

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -3,9 +3,10 @@ import '../assets/styles/Task.css'
 function Task({task}) {
     return (
         <div className='Task-Container'>
-            <h2>Task Name: {task.name}</h2>
-            <p>Task Description: {task.description}</p>
-            <p>Date Created: {task.datecreated}</p>
+            <h2>{task.name}</h2>
+            <button>x</button>
+            <p>Task: {task.description}</p>
+            <p>Date Started: {task.datecreated}</p>
             <p>Date Due: {task.datedue}</p>
         </div>
     )
